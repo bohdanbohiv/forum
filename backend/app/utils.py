504ec3f060e8f97f2906c1c1a93aecab8924f877
Bohdan <bohiv.bohdan@gmail.com>
2025-02-05 15:04:30 +0100

    Refactor to use centralized configuration via Pydantic settings
    Replaced hardcoded values with a `Settings` class based on Pydantic's settings module, enabling centralized management of environment variables. Updated imports and `.gitignore` to integrate the new `.env` loading mechanism. This improves maintainability and security by externalizing sensitive configuration.

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -9,12 +9,9 @@ from jwt.exceptions import InvalidTokenError
 from passlib.context import CryptContext
 from pydantic import ValidationError
 
+from .config import settings
 from .db import SessionDep
 from .models import TokenData, User
-
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-SECRET_KEY = ''
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 oauth2 = OAuth2PasswordBearer(tokenUrl='login/access-token')
@@ -22,15 +19,15 @@ oauth2 = OAuth2PasswordBearer(tokenUrl='login/access-token')
 
 def create_access_token(subject) -> str:
     return jwt.encode({'exp': datetime.now(timezone.utc) + timedelta(
-        minutes=ACCESS_TOKEN_EXPIRE_MINUTES), 'sub': str(subject)}, SECRET_KEY,
-                      ALGORITHM)
+        minutes=settings.access_token_expire_minutes), 'sub': str(subject)},
+                      settings.secret_key, settings.algorithm)
 
 
 def get_current_user(token: Annotated[str, Depends(oauth2)],
                      session: SessionDep) -> User:
     try:
-        token_data = TokenData(
-            **jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM]))
+        token_data = TokenData(**jwt.decode(token, settings.secret_key,
+                                            algorithms=[settings.algorithm]))
     except (InvalidTokenError, ValidationError):
         raise HTTPException(HTTPStatus.UNAUTHORIZED, 'Invalid token',
                             {'WWW-Authenticate': 'Bearer'})
